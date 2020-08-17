fcf1ab41f485dd146eb462ed8b0e233a39e52cc6
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-17 16:58:55 -0400

    created Appointment and BlackList classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,6 @@
 class Artist < ActiveRecord::Base
-    belongs_to :TattooShop
+    belongs_to :tattooshop
     has_many :appointments
     has_many :clients, through: :appointments
     has_many :clients, through: :blacklist
-
 end
---
+++ lib/blacklist.rb
@@ -0,0 +1,4 @@
+class BlackList < ActiveRecord::Base
+    belongs_to :tattooshop
+    belongs_to :artist
+end
