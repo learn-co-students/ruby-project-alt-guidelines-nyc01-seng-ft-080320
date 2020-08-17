1935ef46651cdf01417e080d92b32109ffdf1b9c
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-17 16:36:57 -0400

    update tattooshopclass

--- lib/tattooshop.rb
+++ lib/tattooshop.rb
@@ -1,2 +1,10 @@
-class TattooShop
+class TattooShop < ActiveRecord::Base 
+    has_many :artists
+    has_many :clients 
+    has_many :clients, through: :blacklists 
+    has_many :appointments 
+    has_many :clients, through: :appointments
+    has_many :artists, through: :appointments 
+
+    
 end
