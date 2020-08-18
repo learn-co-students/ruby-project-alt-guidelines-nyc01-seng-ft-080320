fdd8d0d8227748d5ec70aec81d399eba3ab29019
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-18 12:03:21 -0400

    file names corrected; schema created from db:migrate

--- db/migrate/20200818160023_createclient.rb
+++ db/migrate/20200818160023_createclient.rb
@@ -1,4 +1,4 @@
-class CreateClients < ActiveRecord::Migration[5.2]
+class Createclient < ActiveRecord::Migration[5.2]
   def change
     create_table :clients do |t|
       t.string :name
@@ -7,3 +7,4 @@ class CreateClients < ActiveRecord::Migration[5.2]
     end
   end
 end
+
---
+++ db/migrate/20200818160147_createtattooshops.rb
@@ -0,0 +1,4 @@
+class Createtattooshops < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
