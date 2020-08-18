218353d3220f68c26c40764d5252e904a1bd3963
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-18 10:34:28 -0400

    created migration for tattooshop

--- db/migrate/20200818141736_CreateTattooShop.rb
+++ db/migrate/20200818141736_CreateTattooShop.rb
@@ -1,4 +1,9 @@
 class CreateTattooShop < ActiveRecord::Migration[5.2]
+  has_many :blacklists
+  has_many :artists
+  has_many :appointments, through: :artists
   def change
+    create_table :tattooshops do |t|
+      t.string :name
   end
 end
