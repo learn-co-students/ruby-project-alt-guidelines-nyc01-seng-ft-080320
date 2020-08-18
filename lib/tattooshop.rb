bdf42fb829f0cdd0c9cb3dcb798c2eae83e545d0
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-18 15:53:55 -0400

    more seeds defined

--- lib/tattooshop.rb
+++ lib/tattooshop.rb
@@ -1,8 +1,6 @@
 class TattooShop < ActiveRecord::Base 
-    has_many :blacklists
     has_many :appointments 
     has_many :artists
     has_many :clients, through: :blacklists 
     has_many :clients, through: :appointments
-    has_many :artists, through: :appointments 
 end
