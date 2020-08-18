02883d40654531f05d8f8ca291c7afcbd0eeea6d
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-18 10:53:33 -0400

    commiting files

--- lib/blacklist.rb
+++ lib/blacklist.rb
@@ -1,4 +1,5 @@
 class BlackList < ActiveRecord::Base
+    has_many :clients
     belongs_to :tattooshop
     belongs_to :artist
 end
--- lib/client.rb
+++ lib/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base 
+    belongs_to :blacklist
     has_many :appointments 
     has_many :artists, through: :appointments
-    belongs_to :blacklists
 
 end
