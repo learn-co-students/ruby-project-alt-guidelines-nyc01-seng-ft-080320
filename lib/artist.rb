deda49d21920de89e5efb1d539c9f4f301dd2aaa
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-18 10:08:09 -0400

    update1

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,6 @@ class Artist < ActiveRecord::Base
     belongs_to :tattooshop
     has_many :appointments
     has_many :clients, through: :appointments
-<<<<<<< HEAD
     has_many :clients, through: :blacklists
 
-=======
-    has_many :clients, through: :blacklist
->>>>>>> fcf1ab41f485dd146eb462ed8b0e233a39e52cc6
 end
