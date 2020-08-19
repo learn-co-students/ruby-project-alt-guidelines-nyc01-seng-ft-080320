03d379d71cfe78fbc1445e5e71873bb3e4e2b05b
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-19 14:13:22 -0400

    appointment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 Client.destroy_all
 TattooShop.destroy_all
 Artist.destroy_all 
-Blacklist.destroy_all
+Appointment.destroy_all
+
 
 alex = Client.create(name: "Alex", location: "Bronx")
 sarahlee = Client.create(name:"Sarah Lee", location: "Bronx")
@@ -21,9 +22,9 @@ queenstattoo = TattooShop.create(name: "Queens Tattoo", location: "Queens", aver
 hustleparlor = TattooShop.create(name: "Hustle Parlor", location: "Brooklyn", average_review: 3, ambiance: "Upscale", operational_hours: "12PM to 12AM", does_piercings?: true, recommended?: true)
 williamsburger = TattooShop.create(name: "Williamsburger", location: "Brooklyn", average_review: 2, ambiance: "Dirty", operational_hours: "12PM to 7PM", does_piercings?: false, recommended?: false)
 paintshop = TattooShop.create(name: "Paint Shop", location: "Queens", average_review: 3, ambiance: "Cool", operational_hours: "10AM to 8PM", does_piercings?: true, recommended?: true)
-tortoiseshell = TattooShop.create(name: "Tortoise Shell", location: "Manhattan", average_review: 4, ambiance: "Casual", operational_hours: "12PM to 8PM", does_piercings?: false, recommended?: true )
+tortoiseshell = TattooShop.create(name: "Tortoise Shell", location: "Staten Island", average_review: 4, ambiance: "Casual", operational_hours: "12PM to 8PM", does_piercings?: false, recommended?: true )
 applejack = TattooShop.create(name: "Apple Jack", location: "Manhattan", average_review: 3, ambiance: "Cool", operational_hours: "10AM to 9PM", does_piercings?: false, recommended?: true)
-blackhole = TattooShop.create(name: "Black Hole", location: "Queens", average_review: 4, ambiance: "Casual", operational_hours: "12PM to 7PM", does_piercings?: true, recommended?: true)
+blackhole = TattooShop.create(name: "Black Hole", location: "Staten Island", average_review: 4, ambiance: "Casual", operational_hours: "12PM to 7PM", does_piercings?: true, recommended?: true)
 
 
 jack = Artist.create(name: "Jack", style: "traditional", does_piercings?: true, tattoo_shop: brooklyntattoo)
@@ -36,5 +37,15 @@ kim = Artist.create(name: "Kim", style: "black work", does_piercings?: false, ta
 oliver = Artist.create(name: "Oliver", style: "new school", does_piercings?: true, tattoo_shop: gnostic)
 ramon = Artist.create(name: "Ramon", style: "traditional", does_piercings?: false, tattoo_shop: williamsburger)
 cynthia = Artist.create(name: "Cynthia", style: "tribal", does_piercings?: true, tattoo_shop: tortoiseshell)
+theodore = Artist.create(name: "Theodore", style: "new school", does_piercings?: true, tattoo_shop: applejack)
 
-
+appt1 = Appointment.create(date: "September 3, 2020", time: "12PM", description: "Right arm sleeve outline", artist: jack, tattoo_shop: brooklyntattoo, client: joe)
+appt2 = Appointment.create(date: "August 31, 2020", time: "3PM", description: "Butterfly on lower back", artist: dj, tattoo_shop: gnostic, client: paulo)
+appt3 = Appointment.create(date: "August 27, 2020", time: "4PM", description: "Anchor right forearm", artist: omar, tattoo_shop: queenstattoo, client: thelma)
+appt4 = Appointment.create(date: "August 26, 2020", time: "12PM", description: "Right leg sleeve second pass", artist: charlotte, tattoo_shop: paintshop, client: desmond)
+appt5 = Appointment.create(date: "September 12, 2020", time: "4PM", description: "Stomach girlfriend's name", artist: kim, tattoo_shop: brooklyntattoo, client: ruby)
+appt6 = Appointment.create(date: "September 5, 2020", time: "10AM", description: "Lower left leg soccer ball", artist: theodore, tattoo_shop: applejack, client: sarahlee)
+appt7 = Appointment.create(date: "August 30, 2020", time: "6PM", description: "Full back cross and script", artist: omar, tattoo_shop: queenstattoo, client: antonio)
+appt8 = Appointment.create(date: "September 3, 2020", time: "5PM", description: "Left bicep barbed wire", artist: cynthia, tattoo_shop: tortoiseshell, client: francis)
+appt9 = Appointment.create(date: "September 9, 2020", time: "7PM", description: "Left foot infinity symbol", artist: angela, tattoo_shop: bctattoo, client: ruby)
+appt10 = Appointment.create(date: "September 13, 2020", time: "3PM", description: "Right forearm Charizard", artist: oliver, tattoo_shop: gnostic, client: sarah)
