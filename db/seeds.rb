2a35d6116f9761d16507b63f35093f5e6577f985
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-19 11:57:11 -0400

    added ruby user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ sarahlee = Client.create(name:"Sarah Lee", location: "Bronx")
 sarah = Client.create(name:"Sarah", location: "Brooklyn")
 joe = Client.create(name:"Joe", location: "Queens")
 paulo = Client.create(name:"Paulo", location: "Brooklyn")
+ruby = Client.create(name: "Ruby", "Manhattan")
 
 brooklyntattoo = TattooShop.create(name: "Brooklyn Tattoo", location: "Brooklyn", average_review: 4, ambiance: "Casual", operational_hours: "10AM to 8PM", does_piercings?: true, recommended?: true)
 bctattoo = TattooShop.create(name: "B.C. Tattoo", location: "Manhattan", average_review: 3, ambiance: "Cool", operational_hours: "12PM to 6PM", does_piercings?: false, recommended?: true)
