2d540f031b2b81ea79763f99796cf0875d9d12aa
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-20 01:45:04 -0400

    built out artist lookup method. it is 85-90% done. roughly 2/3 of the project functionality remains to be built out

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,97 +1,108 @@
 require 'pry'
 
-# choices = ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"]
-# prompt.select("Welcome to TattedPortal! What neighborhood are you looking for artists in?", choices)
-# prompt.ask("Great! Can you give us your name? We use this information to start setting up your appointment.", required: true)
 
-
-# #TODO
-
-# # what are you interested in getting should equal to the description of appointment
 require 'pry'
-def artist_lookup
-    prompt = TTY::Prompt.new
-        artistname = prompt.ask("What's their name?") do |q|
-                         q.modify :capitalize
-                     end
-                     if Artist.all.find_by name: artistname
-                        whats_their_name_answer = Artist.all.find_by name: artistname
-                        #binding.pry
-                        prompt.yes?("Great! We love #{artistname}! Would you like to make an appointment?")
-                        #need info to make the rest of Appointment.create()
-                        prompt.collect do
-                           description =  key(:description).ask("What are you planning on getting?")
-                           date = key(:date).ask("When were you planning on comming in? MMDDYYY")
-                            Appointment.create(client: Client.all.last, date: date, description: description, artist: whats_their_name_answer, tattoo_shop: whats_their_name_answer.tattoo_shop )
-                            prompt.ask("Thank you and youre all ready to go!")
-                            binding.pry
-                        end
-                     else prompt.ask("We dont know 'em!")
-                        help_search
-                     end
-#                     binding.pry
-            # artists.name.include?(artistname.to_s)
-            # end
-                  
-end
 
-def help_search
-    prompt = TTY::Prompt.new
-    prompt.select("No worries – we'll help you start a new search. How would you like to filter your search?",["Style", "Location", "Also Does Piercings", "Ambiance"])
-end
 
-# def look_for_artist_by_name
-#     prompt = TTY::Prompt.new
-#     answer = prompt.yes?("Cool. Do you know the name of the artist you want?")
-#     answer == "yes"
-#         artist_lookup
-#     answer == "No"
-#          help_search
-    
-# end
-
-#appointmentoptions = ["date", "time", "Describe whay you'd like done", ]
 def make_an_appointment
-    prompt = TTY::Prompt.new
-        stylechosen = prompt.select("What kind of style are you looking for?", ["black work", "traditional", "new school", "tribal", "traditional", "no preference"])
-        artiststring = []
-        artistarray = []
-        Artist.all.select do |artists| 
-            if artists.style == stylechosen
-                artiststring << "#{artists.name} – Does #{artists.style} at #{artists.tattoo_shop.name} in #{artists.tattoo_shop.location}" 
-                artistarray << artists
-                #binding.pry
-            end
-        end
-        chosenartist = prompt.multi_select("Here are some experts in that style. Choose one to make an appointment!", artiststring) 
-        artistarray
+prompt = TTY::Prompt.new
+    
+    stylechosen = prompt.select("What kind of style are you looking for?", ["black work", "traditional", "new school", "tribal", "traditional", "no preference"])
+    
+    artiststring = []
+    artistarray = []
 
-        
- 
-           
+    Artist.all.select do |artists| 
+        if artists.style == stylechosen
+            artiststring << "#{artists.name} – Does #{artists.style} at #{artists.tattoo_shop.name} in #{artists.tattoo_shop.location}" 
+            artistarray << artists
+        end
+    end
+    
+    chosenartist = prompt.multi_select("Here are some experts in that style. Choose one to make an appointment!", artiststring) 
+    artistarray
+    #binding.pry
+end
+
+
+
+def welcoming_new_client
+prompt = TTY::Prompt.new(active_color: :red)
+
+    newclient = prompt.ask("Welcome to TattedPortal! What's your name?", required: true){|q| q.modify :capitalize}
+    Client.create(name:newclient)
+    prompt.ask("Great! Welcome, #{newclient}. Let's get started.")
+    
+    
+    initialpromptoptions = ["Look for an artist", "Make an appointment", "Change appointment"]
+    selectedoption = prompt.select("What would you like to do?", initialpromptoptions)
+    
+    if selectedoption == initialpromptoptions[0] 
+        artist_lookup
+    elsif selectedoption == initialpromptoptions[1]
+        make_an_appointment 
+    elsif selectedoption == initialpromptoptions[2]
+        change_appointment
+    end
+
+end
+
+def artist_lookup
+prompt = TTY::Prompt.new
+pastel = Pastel.new
+            
+    artistname = prompt.ask("Do you know their name? We'll check it against our database of artists!") {|q| q.modify :capitalize}
+                
+        if Artist.all.find_by name: artistname
+            whats_their_name_answer = Artist.all.find_by name: artistname
+            prompt.yes?("Found them! Nice! We love #{artistname}. Would you like to make an appointment with them?")
+            prompt.collect do
+                description = key(:description).ask("Cool. First thing's first: What are you planning on getting?", required: true)
+                date = key(:date).ask("And when were you planning on comming in? Month, day, and year please!", required: true)
+                prompt.ask("Sweet, thanks #{Client.all.last.name}! We're gonna run it back to you so you can let us know if there's anything we missed:")
+                prompt.yes?("Your name is #{Client.all.last.name}, and your artist's name is #{artistname}...")
+                prompt.yes?("And we've got you down for #{date} at #{whats_their_name_answer.tattoo_shop.name} in #{whats_their_name_answer.tattoo_shop.location} and you told us you wanted: #{description}.")
+                newappt = Appointment.new(client: Client.all.last, date: date, description: description, artist: whats_their_name_answer, tattoo_shop: whats_their_name_answer.tattoo_shop)
+                anythingelse = prompt.no?("Great – we just made your appointment. You're on your way to your next tattoo! Is there anything else we can help you with?") 
+                     if anythingelse == true 
+                        prompt.ask("Well alrighty then. Thanks for visiting TattedWorld and see you next time.") 
+                        puts pastel.yellow("Stay golden.")
+                        newappt.save
+                        # at this point, everything is set and we can finally save this appointment instance. however
+                        # if user selects yes help me with something else, the appointment remains in new but not saved. 
+                        # lets not use #create off the bat to avoid having a giant appointment list full of things
+                        # that might not be solid yet.
+                     elsif anythingelse == false 
+                        changeclientappointment = prompt.select("What else can we help you with?", ["Change Appointment", "Nevermind."])
+                            if changeclientappointment == "Change Appointment"
+                                change_appointment 
+                                # change_appointment should ask if they want to change the appointment they just had or see if they can find any other 
+                                # existing appoint of this client...this might entail having to make a sign in feature, since technically each 
+                                # time we run the program, a new client gets created, thus though the same name might show up on the list, it'll be a different
+                                # instance, so technically a whole new person. as it stands, this program doesnt allow for a client to have "past" appointments. 
+                            else puts pastel.yellow("Stay golden.")
+                            end
+                    end
+            end
+        else prompt.ask("Hmm...")
+            prompt.ask("We dont know 'em!")
+            help_search
+    end
 end
 
 def change_appointment 
     prompt = TTY::Prompt.new
+    prompt.ask("lets hope this works!")
+end
 
+def help_search
+prompt = TTY::Prompt.new
+    
+    prompt.ask("No worries, though – we'll help you find the right person for you.")
+    prompt.select("How would you like to filter your search?",["Style", "Location", "Also Does Piercings", "Ambiance"])
 end
 
 
-def welcoming_new_client
-    prompt = TTY::Prompt.new
-    newclient = prompt.ask("Welcome to TattedPortal! What's your name?", required: true)
-    Client.create(name:newclient)
-    puts "Great! Welcome #{newclient}."
-    searchoptions = ["Look for an artist", "Make an appointment", "Change appointment"]
-    selectedoption = prompt.select("What would you like to do?", searchoptions)
-    if selectedoption == searchoptions[0] 
-        artist_lookup
-    elsif selectedoption == searchoptions[1]
-        make_an_appointment 
-    elsif selectedoption == searchoptions[2]
-        change_appointment
-    end
-end
 
 
 
